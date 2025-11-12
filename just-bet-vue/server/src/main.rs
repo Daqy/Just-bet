d3cfeea37ff82ec80fe707f5d77ef4974f19c14b
Daqy <daqy@Klaidas.localdomain>
2025-11-12 13:41:53 +0000

    backend: install axum, tokio and more
    - remove target direcotry
    - setup hello world request

--- just-bet-vue/server/src/main.rs
+++ just-bet-vue/server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-  println!("Hello, world!");
+use axum::{routing, Router};
+
+#[tokio::main]
+async fn main() {
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  
+  let app = Router::new().route("/", routing::get(|| async {"hell world"}));
+
+  println!("server listen on port : {}", listener.local_addr().unwrap());
+  
+  axum::serve(listener, app).await.unwrap();
 }
