0e438e011ab6cd451ccb5112eed00e7911859b66
Daqy <klaidas.predeinas1@gmail.com>
2026-06-13 16:52:25 +0100

    config: update vite config to proxy ws

--- just-bet-vue/client/vite.config.ts
+++ just-bet-vue/client/vite.config.ts
@@ -21,6 +21,13 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false
+      },
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/ws/, '/api')
       }
     }
   }
